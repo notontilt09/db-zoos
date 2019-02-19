ca146d81bb2e5a36b09c2bc7408246b4425c6472
Daniel Weinman <namniewd@gmail.com>
2019-02-19 14:47:18 -0500

    wrote all endpoints, mvp complete

--- index.js
+++ index.js
@@ -71,6 +71,26 @@ server.delete('/api/zoos/:id', async (req, res) => {
   }
 });
 
+server.put('/api/zoos/:id', async (req, res) => {
+  try {
+    const count = await db('zoos')
+      .where({ id: req.params.id })
+      .update(req.body);
+
+    if (count) {
+      const zoo = await db('zoos')
+        .where({ id: req.params.id })
+        .first();
+      
+      res.status(200).json(zoo)
+    } else {
+      res.status(404).json({ message: 'Record with that id not found'})
+    }
+  } catch (error) {
+    res.status(500).json(error)
+  }
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
